abc0ce48fd1adb3d53510f8b628d044b072ce63e
ARL Tech <tech@agresearchlabs.com>
2026-06-01 16:05:54 +0530

    fix: add Platform.isAndroid/isIOS/etc to web stub

--- lib/core/utils/io_types_web.dart
+++ lib/core/utils/io_types_web.dart
@@ -12,4 +12,10 @@ class File {
 
 class Platform {
   static const String pathSeparator = '/';
+  static const bool isAndroid = false;
+  static const bool isIOS = false;
+  static const bool isMacOS = false;
+  static const bool isWindows = false;
+  static const bool isLinux = false;
+  static const bool isFuchsia = false;
 }
